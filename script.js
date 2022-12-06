a8f64a86557bf58a75fb95e0ccca11b407aa58ed
Elias Kupeczki <github@kunoxy.ch>
2022-12-06 20:57:23 +0100

    button flip

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function updateClock() {
-    let time = new Date().toLocaleString();
-    document.getElementById('clock').innerHTML = time;
+  let time = new Date().toLocaleString();
+  document.getElementById("clock").innerHTML = time;
 }
 
 setInterval(updateClock, 1000);
+
+function myFunction() {
+  var element = document.getElementById("leftRight");
+  element.classList.toggle("rotate");
+}
