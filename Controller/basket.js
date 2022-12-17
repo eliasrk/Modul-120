abd17df490583b55c1f969702608ae5b2f7b9253
eliasrk <github@kunoxy.ch>
2022-12-17 10:57:28 +0100

    local storage

--- Controller/basket.js
+++ Controller/basket.js
@@ -1,2 +1,47 @@
-const cookieValues = document.cookie.split(";").map((c) => c.trim());
-console.log(cookieValues);
+localStorage.getItem("direction");
+localStorage.getItem("von");
+console.log(localStorage.getItem("direction"));
+console.log(localStorage.getItem("von"));
+
+var firstminus = document.getElementById("first-");
+var firstplus = document.getElementById("first+");
+var secondminus = document.getElementById("second-");
+var secondplus = document.getElementById("second+");
+var first = document.getElementById("first");
+var second = document.getElementById("second");
+var firstcount = 0;
+var secondcount = 0;
+firstminus.addEventListener("click", function () {
+  if (firstcount > 0) {
+    firstcount--;
+    first.innerText = "";
+    first.append(" : " + firstcount);
+    console.log(firstcount);
+  }
+});
+
+firstplus.addEventListener("click", function () {
+  firstcount++;
+  first.innerText = "";
+  first.append(" : " + firstcount);
+  console.log(firstcount);
+});
+
+secondminus.addEventListener("click", function () {
+  if (secondcount > 0) {
+    secondcount--;
+
+    second.innerText = "";
+    second.append(" : " + secondcount);
+    console.log(secondcount);
+  }
+});
+
+secondplus.addEventListener("click", function () {
+  secondcount++;
+  second.innerText = "";
+  second.append(" : " + secondcount);
+  console.log(secondcount);
+});
+
+function pricecalc() {}
